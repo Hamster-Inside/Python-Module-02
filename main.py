e07f28c86635df7d1adac7dce1d9e74a930c31a2
Arek <abudkowski@interia.pl>
2022-10-26 06:55:48 +0200

    Starting files

---
+++ main.py
@@ -0,0 +1,9 @@
+# Password:
+# * 8 signa
+# * one number
+# * one special sign
+# * lowercase and uppercase letters
+# check if password is not on site 'have i been pwnd'
+# write passwords in passwords.txt
+# write safe passwords to safe.txt
+# use logger to save in file
